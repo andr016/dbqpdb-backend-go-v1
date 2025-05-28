3fe2cc121d6102ced33908eb3d450114c825ac94
andr016 <81148405+andr016@users.noreply.github.com>
2025-05-28 17:42:53 +0300

    auth wip

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
+	"time"
 
 	"dbqpdb-backend-go-v1/config"
 	"dbqpdb-backend-go-v1/folder"
@@ -19,8 +20,10 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 
+	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 // Flags
@@ -303,6 +306,45 @@ func groupAdd(c *fiber.Ctx) error {
 	return nil
 }
 
+func login(c *fiber.Ctx) error {
+	user := c.FormValue("user")
+	pass := c.FormValue("pass")
+
+	// Throws Unauthorized error
+	if user != "john" || pass != "doe" {
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+
+	// Create the Claims
+	claims := jwt.MapClaims{
+		"name":  "John Doe",
+		"admin": true,
+		"exp":   time.Now().Add(time.Hour * 72).Unix(),
+	}
+
+	// Create token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Generate encoded token and send it as response.
+	t, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return c.JSON(fiber.Map{"token": t})
+}
+
+func accessible(c *fiber.Ctx) error {
+	return c.SendString("Accessible")
+}
+
+func restricted(c *fiber.Ctx) error {
+	user := c.Locals("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	name := claims["name"].(string)
+	return c.SendString("Welcome " + name)
+}
+
 func main() {
 	flag.Parse()
 	c, err = config.LoadConfig("config/config.json")
@@ -335,6 +377,21 @@ func main() {
 
 	app := fiber.New()
 	app.Static("/uploads", "./uploads")
+
+	// AUTH TEST
+	// Login route
+	app.Post("/login", login)
+
+	// Unauthenticated route
+	app.Get("/", accessible)
+
+	// JWT Middleware
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: jwtware.SigningKey{Key: []byte("secret")},
+	}))
+
+	// Restricted Routes
+	app.Get("/restricted", restricted)
 
 	app.Use(cors.New(cors.Config{
 		// Allow all origins to access the resources
